292fb89d2a8cdaeaaf29bd01d2d593142cec4014
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-03-29 06:36:47 -0300

    aula sobre condicionais

--- README.md
+++ README.md
@@ -2,10 +2,41 @@
 ### Link do desafio abaixo:
 - https://www.torneseumprogramador.com.br/cursos/desafio_rust/aulas
 
-## Prompt GPT 27/03/2024
+# Prompt GPT 27/03/2024
 
 Tipo de dados
 - https://chat.openai.com/share/a8784f57-5faf-42af-826f-415ec202db66
 
 Tipos de memória (Static, Stack, Heap)
 - https://drive.google.com/file/d/1vT08-xtgAYM-9rZABqQojI-9QPv2E4w9/view?usp=sharing
+
+
+# Aula 29/03/2024
+### Condicionais em Rust
+
+Em Rust, as estruturas condicionais são usadas para executar diferentes partes do código baseado em determinadas condições.
+
+## if
+
+A expressão `if` é a mais simples estrutura condicional, que avalia uma condição e executa um bloco de código se a condição for verdadeira.
+
+```rust
+if condicao {
+    // código a ser executado se `condicao` for verdadeira
+}
+```
+
+## else e else if
+
+Se a condição do `if` não for verdadeira, você pode usar `else` para especificar um bloco de código a ser executado. O `else if` permite testar condições adicionais.
+
+```rust
+if condicao {
+    // código se `condicao` for verdadeira
+} else if outra_condicao {
+    // código se `outra_condicao` for verdadeira
+} else {
+    // código se nenhuma das condições anteriores for verdadeira
+}
+```
+
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
-
-fn retorno_da_funcao() -> i8{
-    // return 1; // opção 1
-    1  // opção 2
-}  
-
-fn escrever_algo(){ // função sem retorno
-    println!("Oiiiiiiii");
-}
-
-fn funcao_com_parametros(valor: String, numero: i32) -> String{
-    format!("{} - {}", valor, numero)
-}
+use std::io;
+use chrono::{Utc, Datelike};
+use regex::Regex;
 
 fn main() {
-    let x = retorno_da_funcao();
-    escrever_algo();
-    escrever_algo();
-    escrever_algo();
+    /*
+    Dado que eu tenha um ano de nascimanto, e faço a subtração pelo ano atual,
+    Então devo ter o valor da idade da pessoa
+    */
 
-    println!("{}", x);
+    println!("Digite a data de seu nascimento (dd/mm/yyyy):");
 
-    let valor_concatenado = funcao_com_parametros("Danilo".to_string(), 40);
-    println!("{}", valor_concatenado);
+    let mut data_nascimento = String::new();
+    io::stdin().read_line(&mut data_nascimento).expect("Falha ao ler entrada");
+
+    let data_regex = Regex::new(r"^\d{2}\/\d{2}\/\d{4}$").unwrap();
+    if ! data_regex.is_match(&data_nascimento.trim()) {
+        println!("A data que você digitou ({}) não está no formato permitido", data_nascimento.trim());
+        return;
+    }
+
+    let data_split: Vec<&str> = data_nascimento.split("/").collect();
+
+    let dia_usuario: u32 = data_split[0].trim().parse().expect("Por favor, digite um número válido!");
+    let mes_usuario: u32 = data_split[1].trim().parse().expect("Por favor, digite um número válido!");
+    let ano_usuario: i32 = data_split[2].trim().parse().expect("Por favor, digite um número válido!");
+
+    let data_atual = Utc::now();
+    let dia_atual: u32 = data_atual.day();
+    let mes_atual: u32 = data_atual.month();
+    let ano_atual: i32 = data_atual.year();
+
+    let mut idade_usuario :u8 = (ano_atual - ano_usuario) as u8;
+
+    if mes_usuario > mes_atual {
+        idade_usuario -= 1;
+    }
+    else if dia_usuario > dia_atual {
+        idade_usuario -= 1;
+    }
+
+    println!("A sua idade é: {}", idade_usuario);
 }
