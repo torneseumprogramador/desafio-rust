556f3baa205262af58888d7750ef2ff8d6af03ab
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-04-08 05:13:39 -0300

    versao aula passada

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,31 @@ fn main() {
     // fn main(){
     /*
     amanha
-    - persistencia
+    - persistencia mysql
+    
+    proximos
+    - tratamento de erros
+    - traits (interfaces)
+    - POO - Polimorfismo
+    - Generics
+    - ORM trabalhando com Generics
+    - uso de ORMs existentes
+        - persistencia mysql
+        - persistencia postgres
+        - persistencia mongodb
+        - persistencia sqlite (db local)
+    - lifetimes
+    - metaprogramação - Macros ...
+    - Testes
+    - Programação funcional vs POO vs programação estruturada
+    - Concorrencia e paralelismo
+    - Programação Asyncrona
+    - Programação Web
+        - render server side
+        - API's
+    - Docs
+    - redis - Cache ou chave/valor
+    - elasticsearch - Indexação e busca
     */
 
     menu::carregar();
